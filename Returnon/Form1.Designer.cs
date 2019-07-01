66c915d472677fb68833510583f0fd3814b53c46
Nastya Utyakaeva <utyakaeva.anastasia@yandex.ru>
2019-07-01 14:23:38 +0800

    tt

---
+++ Returnon/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Returnon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SSBox = new System.Windows.Forms.TextBox();
+            this.CPBox = new System.Windows.Forms.TextBox();
+            this.SABox = new System.Windows.Forms.TextBox();
+            this.SKBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SSBox
+            // 
+            this.SSBox.Location = new System.Drawing.Point(184, 41);
+            this.SSBox.Name = "SSBox";
+            this.SSBox.Size = new System.Drawing.Size(310, 20);
+            this.SSBox.TabIndex = 0;
+            // 
+            // CPBox
+            // 
+            this.CPBox.Location = new System.Drawing.Point(184, 68);
+            this.CPBox.Name = "CPBox";
+            this.CPBox.Size = new System.Drawing.Size(310, 20);
+            this.CPBox.TabIndex = 1;
+            // 
+            // SABox
+            // 
+            this.SABox.Location = new System.Drawing.Point(184, 115);
+            this.SABox.Name = "SABox";
+            this.SABox.Size = new System.Drawing.Size(310, 20);
+            this.SABox.TabIndex = 2;
+            // 
+            // SKBox
+            // 
+            this.SKBox.Location = new System.Drawing.Point(197, 151);
+            this.SKBox.Name = "SKBox";
+            this.SKBox.Size = new System.Drawing.Size(292, 20);
+            this.SKBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Капитал";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Риск в пунктах";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(9, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Риск в % от капитала * 0,01.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(184, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Стоимость пункта для 1 контракта";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(264, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Введите экономические показатели организации:";
+            // 
+            // OKButton
+            // 
+            this.OKButton.Location = new System.Drawing.Point(200, 196);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(105, 43);
+            this.OKButton.TabIndex = 9;
+            this.OKButton.Text = "Рассчитать объем позиции";
+            this.OKButton.UseVisualStyleBackColor = true;
+            this.OKButton.Click += new System.EventHandler(this.OKButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 291);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(480, 199);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Объем позиции";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 253);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(74, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(337, 256);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Размер убытка";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(423, 253);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(66, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 516);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SKBox);
+            this.Controls.Add(this.SABox);
+            this.Controls.Add(this.CPBox);
+            this.Controls.Add(this.SSBox);
+            this.Name = "Form1";
+            this.Text = "Объем позиции при выходе на фондовый рынок";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SSBox;
+        private System.Windows.Forms.TextBox CPBox;
+        private System.Windows.Forms.TextBox SABox;
+        private System.Windows.Forms.TextBox SKBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Returnon/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Returnon
+{
+    public partial class Form1 : Form
+    {
+        public double SS;
+        public double CP;
+        public double SA;
+        public double SK;
+        public double f;
+        public double ROM;
+        public double ROE;
+        public double ROA;
+
+        public bool C1 = false;
+        public bool C2 = false;
+
+        public bool C5 = false;
+      
+        string attr;
+      
+        bool ended = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OKButton_Click(object sender, EventArgs e)
+        {
+            C1 = false;
+            C2 = false;
+            
+            
+            C5 = false;
+           
+
+            
+          
+            ended = false;
+            SS = Convert.ToDouble(SSBox.Text);//капитал
+            CP = Convert.ToDouble(CPBox.Text);//риск в пунктах
+            SA = Convert.ToDouble(SABox.Text);//риск в % от капитала * 0,01
+            SK = Convert.ToDouble(SKBox.Text);//стоимость пункта для одного контракта
+            
+            
+            ROM = (SS * (SA *0.01) )/( CP * SK);
+            textBox1.Text = ROM.ToString();
+            ROA = CP * SK;
+            f = (ROA * SA) / SS;
+            textBox3.Text = ROA.ToString();
+            attr = "Объем позиции";
+            while (!ended)
+            {
+                Cycle(attr);
+            }
+        }
+
+        public void Cycle(string atr)
+        {
+            switch (atr)
+            {
+                case "Объем позиции":
+                    if (SA > f)
+                    {
+                        C1 = true;
+                        richTextBox1.Text = "Если выход бизнеса на фондовый рынок окажется неудачным, размер убытка составит: " + ROA +" что равняется " + f + " % от капитала.Из-за округления объема позиции возникла погрешность, тем не менее, видно, что формула позволяет «зашить» в сделку заранее известный и допустимый уровень риска.";
+                    }
+                    else
+                    {
+                        C2 = true;
+                        richTextBox1.Text = "Выход на фондовый рынок не возможен.";
+
+                    }
+                    attr = "ROE";
+                    break;
+       
+                
+                default:
+                    ended = true;
+                    break;
+                    
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
